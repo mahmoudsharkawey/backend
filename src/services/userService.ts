5f2c08ef99d4a97c2100410ddbd7cd3da9102108
__Rawan__ <rawan156hesham@gmail.com>
2024-09-08 14:10:16 +0300

    modified user responses

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,7 +2,6 @@ import userModel from "../models/userModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
 interface RegisterParams {
   firstName: string;
   lastName: string;
@@ -31,7 +30,13 @@ export const register = async ({
   });
   await newUser.save();
 
-  return { data: generateJWT({ firstName, lastName, email }), statusCode: 200 };
+  return {
+    data: {
+      token: generateJWT({ firstName, lastName, email }),
+      name: firstName,
+    },
+    statusCode: 200,
+  };
 };
 
 interface LoginParams {
@@ -43,24 +48,27 @@ export const login = async ({ email, password }: LoginParams) => {
   const findUser = await userModel.findOne({ email });
 
   if (!findUser) {
-    return { data: "Incorrect email or password!", statusCode: 400 };
+    return { data: "Incorrect email", statusCode: 400 };
   }
 
   const passwordMatch = await bcrypt.compare(password, findUser.password);
   if (passwordMatch) {
     return {
-      data: generateJWT({
-        email,
-        firstName: findUser.firstName,
-        lastName: findUser.lastName,
-      }),
+      data: {
+        token: generateJWT({
+          email,
+          firstName: findUser.firstName,
+          lastName: findUser.lastName,
+        }),
+        name: findUser.firstName,
+      },
       statusCode: 200,
     };
   }
 
-  return { data: "Incorrect email or password!", statusCode: 400 };
+  return { data: "Incorrect password!", statusCode: 400 };
 };
 
 const generateJWT = (data: any) => {
-  return jwt.sign(data, process.env.JWT_SECRET || '');
+  return jwt.sign(data, process.env.JWT_SECRET || "");
 };
